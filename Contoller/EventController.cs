411f7180fa223d3de25fe168632e11264d86b98f
EvanBittar <evanking621@gmail.com>
2026-04-04 13:43:21 +0300

    Change GetEvent to SearchEvents

--- Contoller/EventController.cs
+++ Contoller/EventController.cs
@@ -14,14 +14,15 @@ namespace Eventify_High_Performance_Event_Management_API.Contoller
         {
             _dapper = new DataContext(configuration);
         }
-        [HttpGet("GetEvents")]
-        public async Task<IEnumerable<dynamic>> GetEvents()
+        [HttpGet("SearchEvents")]
+        public async Task<IEnumerable<dynamic>> SearchEvents(string? title = null)
         {
             string sql = @"SELECT e.*,c.NameCategory 
                 FROM Event.Events AS e 
-                JOIN Event.Categories AS c ON e.CategoryId = c.CategoryId";
+                JOIN Event.Categories AS c ON e.CategoryId = c.CategoryId
+                WHERE (@Title IS NULL OR e.Title LIKE '%' + @Title + '%')";
 
-            return await _dapper.LoadData<dynamic>(sql);
+            return await _dapper.LoadData<dynamic>(sql, new { Title = title });
         }
         [HttpPost("AddEvent")]
         public async Task<IActionResult> AddEvent(EventToAddDto eventToAddDto)

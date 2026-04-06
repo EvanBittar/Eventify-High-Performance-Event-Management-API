571b869022da9a83513f6228af2735c7b78e95e0
EvanBittar <evanking621@gmail.com>
2026-04-06 20:07:42 +0300

    nothing to commit

--- obj/Debug/net10.0/Eventify-High-Performance-Event-Management-API.AssemblyInfo.cs
+++ obj/Debug/net10.0/Eventify-High-Performance-Event-Management-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eventify-High-Performance-Event-Management-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62511a039a50c49c14feb8ab9fab6d7944e1d1b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+411f7180fa223d3de25fe168632e11264d86b98f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eventify-High-Performance-Event-Management-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eventify-High-Performance-Event-Management-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

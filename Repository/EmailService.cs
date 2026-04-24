d484bbd39c5d3a68f8b7406225ce00c55300f13d
EvanBittar <evanking621@gmail.com>
2026-04-24 11:17:09 +0300

    chaneg to send for real email

--- Repository/EmailService.cs
+++ Repository/EmailService.cs
@@ -8,25 +8,32 @@ public class EmailService(IConfiguration config) : IEmailService
 
     public async Task SendEmailAsync(string toEmail, string subject, string body)
     {
-        var emailSettings = _config.GetSection("EmailSettings");
-        
-        using var client = new SmtpClient(emailSettings["SmtpServer"])
+        try
         {
-            Port = int.Parse(emailSettings["Port"]!),
-            Credentials = new NetworkCredential(emailSettings["SenderEmail"], emailSettings["Password"]),
-            EnableSsl = true,
-        };
+            var emailSettings = _config.GetSection("EmailSettings");
 
-        var mailMessage = new MailMessage
+            using var client = new SmtpClient(emailSettings["SmtpServer"])
+            {
+                Port = int.Parse(emailSettings["Port"]!),
+                Credentials = new NetworkCredential(emailSettings["SenderEmail"], emailSettings["Password"]),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(emailSettings["SenderEmail"]!, emailSettings["SenderName"]),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(toEmail);
+
+            await client.SendMailAsync(mailMessage);
+        }
+        catch (Exception ex)
         {
-            From = new MailAddress(emailSettings["SenderEmail"]!, emailSettings["SenderName"]),
-            Subject = subject,
-            Body = body,
-            IsBodyHtml = true 
-        };
-        
-        mailMessage.To.Add(toEmail);
-
-        await client.SendMailAsync(mailMessage);
+            Console.WriteLine($"Wronge to send email: {ex.Message}");
+        }
     }
 }
